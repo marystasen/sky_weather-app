fc5d6f2f9b8a5b3211f40e91e0449020e0c0036e
Mary <stasenkmary@gmail.com>
2023-06-12 00:44:41 +0100

    showed a current: date, time, weather description, temperature, humidity, wind

---
+++ src/app.js
@@ -0,0 +1,54 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
+}
+function displayTemperature(responce) {
+  console.log(responce);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
+  temperatureElement.innerHTML = Math.round(responce.data.temperature.current);
+  cityElement.innerHTML = responce.data.city;
+  descriptionElement.innerHTML = responce.data.condition.description;
+  humidityElement.innerHTML = responce.data.temperature.humidity;
+  windElement.innerHTML = Math.round(responce.data.wind.speed);
+  dateElement.innerHTML = `Last updated:
+   ${formatDate(responce.data.time * 1000)}`;
+  iconElement.setAttribute("src", responce.data.condition.icon_url);
+  iconElement.setAttribute("alt", responce.data.condition.description);
+}
+
+let apiKey = "cf5ea9841oaacf10033cdea44t6f44bb";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=London&key=${apiKey}&units=metric`;
+
+console.log(apiUrl);
+axios.get(apiUrl).then(displayTemperature);
